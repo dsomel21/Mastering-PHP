962b7cc37662d941b70f8a9f7ab682c135e90356
dsomel21 <dsomel21@gmail.com>
2017-03-06 16:27:09 -0500

    Post request working well

--- app/Http/Controllers/AlbumsController.php
+++ app/Http/Controllers/AlbumsController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Album;
+use App\Artist;
 
 class AlbumsController extends Controller {
     
-    public function store(Request $request){
+    public function store(Request $request, Artist $artist){
+    	$album = new Album;
+    	$album->album_name = $request->album_name;
+
+    	$artist->albums()->save($album);
+
+    	return back();
+
 			// return 'tonka';
-    	return $request->all();
+    	// return $request->all();
     }
 }
